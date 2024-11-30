837eb3791792a75ee3518e0a0d454a5fe3fb4093
Nam sanghyeok <simian114@gmail.com>
2024-11-30 18:13:11 +0900

    init accounts app

--- config/settings/settings.py
+++ config/settings/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "playground.blog.apps.BlogConfig",
+    "playground.accounts.apps.AccountsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
